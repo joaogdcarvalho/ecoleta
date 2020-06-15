eadc422c90b93c5d9b1730c74546f6a9d89dcaba
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-06-14 23:24:31 -0300

    next level week - aula 5 | alteracao migration - points

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -1,7 +1,6 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-    // CRIAR A TABELA
     return knex.schema.createTable('points', table => {
         table.increments('id').primary();
         table.string('image').notNullable();
@@ -16,6 +15,5 @@ export async function up(knex: Knex) {
 }
 
 export async function down(knex: Knex) {
-    // VOLTAR ATRÁS (DELETAR A TABELA)
-    return knex.schema.dropTable('point');
+    return knex.schema.dropTable('points');
 }
