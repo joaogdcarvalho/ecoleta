53158dbe8b808e84844a8a790bfbd9878cb2bf7d
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-06-15 22:55:44 -0300

    next level week - aula 5 | alteracao migration - points

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -11,7 +11,7 @@ export async function up(knex: Knex) {
         table.decimal('longitude').notNullable();
         table.string('city').notNullable();
         table.string('uf', 2).notNullable();
-    })
+    });
 }
 
 export async function down(knex: Knex) {
