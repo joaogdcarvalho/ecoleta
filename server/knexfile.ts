4b9d09df709eb1609f0828674452bb4c9b56db8d
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-06-16 23:25:25 -0300

    next level week - aula 5 | modificacao knexfile

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -3,9 +3,9 @@ import path from 'path';
 module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
     },
-    migrations:{
+    migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
     seeds: {
