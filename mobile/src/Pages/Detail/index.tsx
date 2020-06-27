5698a0d5e616bc6774c75ad3b93f6f8ab2a8ff2f
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-06-27 00:28:56 -0300

    next level week - aula 5 | remocao arquivos gitignore - mobile

--- mobile/src/Pages/Detail/index.tsx
+++ mobile/src/Pages/Detail/index.tsx
@@ -13,6 +13,7 @@ interface Params {
 interface Data {
   point: {
     image: string;
+    image_url: string;
     name: string;
     email: string;
     whatsapp: string;
@@ -65,7 +66,7 @@ const Detail = () => {
 
                 <Image
                     style={styles.pointImage}
-                    source={{uri: data.point.image }}
+                    source={{uri: data.point.image_url }}
                 />
                 <Text style={styles.pointName}>{data.point.name}</Text>
                 <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
